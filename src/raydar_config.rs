6c77871e93a408b92255f2b3771267e5a0de9f97
foxbecoding <foxbecoding@gmail.com>
2024-12-27 15:50:11 -0600

    add struct props

--- src/raydar_config.rs
+++ src/raydar_config.rs
@@ -1,2 +1,9 @@
+use config::{Config, ConfigError as ConfigBuildError, File};
+use serde::Deserialize;
+
 #[derive(Deserialize, Clone, Debug)]
-pub struct RaydarConfig {}
+pub struct RaydarConfig {
+    pub swap_amount: f64,
+    pub swap_fee_amount: f64,
+    pub swap_hold_time: u64,
+}
